e55756e2e702530a87719ed2098bc0e6d63dec07
menimen <menash9288@gmail.com>
2019-09-28 09:49:20 +0300

    finished mission 1 in ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include <ctype.h>
+#include <string.h>
 #include "ex2utils.h"
+void mission1();
+void mission2();
+void mission3();
 void main() {
     int choose = 4;
     char buf[Max];
@@ -12,14 +16,97 @@ void main() {
     }
     switch(choose) {
         case 1:
+            mission1();
             break;
         case 2:
+            mission2();
             break;
         case 3:
+            mission3();
             break;
         default:
             break;
 
     }
+
+}
+void mission1() {
+    char str[Max];
+    int i = 0,count = 0,trigger = 0, squaretrigger = 0, brackettrigger = 0;
+    printf("Please enter four chars:\n");
+    fgets(str, Max, stdin);
+    for (i = 0; str[i]!='\n'; i++)  {//trim string from spaces
+        if (str[i] != ' '){
+            str[count++] = str[i];
+        }
+    }
+    str[count] = '\0';
+    count = 0;
+    if(strlen(str)!= 4) {//check that only 4 characters entered
+        printf("Bad structure\n");
+        printf("%d", strlen(str));
+        trigger++;
+    }
+    for (i = 0; str[i] && trigger == 0; i++)  {
+        if (str[i] == 'a' || str[i] == '[' || str[i] == ']' || str[i] == '(' || str[i] == ')'){
+            count++;
+        }
+    }
+    if(count != (strlen(str)) && trigger == 0) {
+        printf("bad input\n");
+        trigger = 1;
+    }
+    if(trigger == 0) {
+            for(i = 0; str[i]!='\0' && trigger == 0; i++) {
+                if(str[i] == '[') {
+                    if (squaretrigger > 0 || brackettrigger > 0) {
+                        printf("bad structure\n");
+                        trigger++;
+                    }
+                    else {
+                        squaretrigger++;
+                    }
+                }
+                if(str[i] == '(') {
+                    if(brackettrigger > 0) {
+                        printf("bad structure\n");
+                        trigger++;
+                    }
+                    brackettrigger++;
+                }
+                if(str[i] == ')') {
+                    if(brackettrigger > 0) {
+                        brackettrigger--;
+                    }
+                    else {
+                        printf("bad structure\n");
+                        trigger++;
+                    }
+                }
+                if(str[i] == ']') {
+                    if(squaretrigger > 0) {
+                        squaretrigger--;
+                    }
+                    else {
+                        printf("bad structure\n");
+                        trigger++;
+                    }
+                }
+        }
+        if((brackettrigger > 0 || squaretrigger > 0) && trigger == 0) {
+            printf("bad structure\n");
+            trigger++;
+        }
+        if(trigger == 0){
+            printf("OK");
+        }
+    }
+}
+void mission2() {
+
+return;
+}
+void mission3() {
+return;
 
 }
