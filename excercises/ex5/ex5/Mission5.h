4a19ac70cf4ea3566b5a4a01b0caf2030cf337ad
menimen <menash9288@gmail.com>
2019-10-17 18:59:46 +0300

    finished mission5 in ex5

--- excercises/ex5/ex5/Mission5.h
+++ excercises/ex5/ex5/Mission5.h
@@ -3,11 +3,17 @@
 *Student ID:XXXX
 *Submit Info:XXXX
 *Exercise name: exXXXX
-******************************************/ 
+******************************************/
 
 #ifndef _MISSION5_H_
 #define _MISSION5_H_
+#define MAXBOARDSIZE 25
+#include <stdio.h>
 void mission5();
-void loadCastleBoard(char mat[][/*Complete..*/], int rows, int cols);
-int getMinLength(char mat[][/*Complete..*/], int r, int c, int goalR, int goalC, int n);
+void loadCastleBoard(char mat[][MAXBOARDSIZE], int rows, int cols);
+int getMinLength(char mat[][MAXBOARDSIZE], int r, int c, int goalR, int goalC, int n);
+int getMinLengthhelper(char mat[][MAXBOARDSIZE], int startX, int startY, int goalX, int goalY, int n, int count);
+int Min(int up, int down, int n);
+void loadCastleBoardhelper(char mat[][MAXBOARDSIZE],int i, int rows, int cols);
+void printBoardM(char mat[][MAXBOARDSIZE], int n);
 #endif
--- excercises/ex5/ex5/ex5.c
+++ excercises/ex5/ex5/ex5.c
@@ -9,7 +9,7 @@
 #include "Mission2.h"
 #include "Mission3.h"
 #include "Mission4.h"
-/*#include "Mission5.h"*/
+#include "Mission5.h"
 #include <stdio.h>
 
 int main()
@@ -30,8 +30,8 @@ int main()
 			break;
 		case 4: mission4();		// Run the appropriate mission
 			break;
-		/*case 5: mission5();		// Run the appropriate mission
-			break;*/
+		case 5: mission5();		// Run the appropriate mission
+			break;
 		default: // do nothing
 			break;
 		}
